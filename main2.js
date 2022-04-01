0a6459abe8353aff8678171918c052dc966894ac
HeatoNdude <84017829+HeatoNdude@users.noreply.github.com>
2022-04-01 11:16:01 +0530

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    document.getElementById("popmenu").style.width = "300px";
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    
+    menuOpen = false;
+    document.getElementById("popmenu").style.width = "0px";
+  }
+});
