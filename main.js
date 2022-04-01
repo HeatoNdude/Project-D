1f9c9c65cda7a02c8e33b9898bbc947bba93b19c
HeatoNdude <84017829+HeatoNdude@users.noreply.github.com>
2022-04-01 11:15:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,20 @@ let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
+    
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
+    
     menuOpen = false;
+    
   }
 });
+
+// $(document).ready(function() {  
+//   $('#btnValue').click(function() {  
+//    $("#divkarea").html("");  
+//    var content = tinymce.get("txtarea").getContent();  
+//    $("#divkarea").html(content);  
+//   });  
+//  });  
