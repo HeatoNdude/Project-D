8cfffabe3a036127971154c604de58d88ef63185
HeatoNdude <84017829+HeatoNdude@users.noreply.github.com>
2022-05-17 10:19:17 +0530

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,13 +1,13 @@
-const menuBtn = document.querySelector('.menu-btn');
+const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open');
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
     document.getElementById("popmenu").style.width = "300px";
     menuOpen = true;
   } else {
-    menuBtn.classList.remove('open');
-    
+    menuBtn.classList.remove("open");
+
     menuOpen = false;
     document.getElementById("popmenu").style.width = "0px";
   }
